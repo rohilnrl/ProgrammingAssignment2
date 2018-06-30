c04c28c20ace0d906d9a930c8b565411f352b669
Rohil Narula <rohilnrl@gmail.com>
2018-06-30 22:05:21 +0530

    Added documentation

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,9 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix when called accepts a matrix as argument
+## and assigns 4 functions to get and set the matrix and
+## it's inverse. cacheSolve solves for the inverse of a matrix.
 
-## Write a short comment describing this function
-
+## Accepts a matrix and creates 4 functions to compute
+## and display its inverse.
 makeCacheMatrix <- function(x = matrix()) {
   x <- x
   i <- NULL
@@ -21,8 +22,9 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
-
+## Takes a list of the 4 functions (returned by the above
+## function) and calculates the matrix. If already calculated
+## it returns it from the cache.
 cacheSolve <- function(x, ...) {
   i <- x$get_inverse()
   if (!is.null(i)) {
